615f7eb7ce1a048a8f1ccd89e8ccca43294d7b69
JBSellman <32461430+JBSellman@users.noreply.github.com>
2021-02-01 18:10:11 +0000

    qcut_func now takes a series and an integre and produces a list of bin names. This list needs to be converted into a series/dictionary and appended to the df

--- qcut_func.py
+++ qcut_func.py
@@ -1,4 +1,35 @@
 # def qcut_james(x,q,labels):
-def qcut_james(x):
-    ret = x +1
-    return ret
+import math as m
+
+def qcut_james(x, q):
+    ret = x.sort_values(ascending=False)
+    col_len = len(ret)
+    quart_len = m.floor(col_len / q)
+    quart_modulo = col_len % q
+    ret_categories = []
+
+    bin_boundary = list()
+    bin_cumulative = 0
+    for i in range(q):
+        if i + 1 <= quart_modulo:
+            bin_size = quart_len + 1
+        else:
+            bin_size = quart_len
+
+        bin_cumulative = bin_cumulative + bin_size
+        bin_boundary.append(bin_cumulative)
+
+
+    bin_no = 1
+    for i in range(col_len):
+        if i <= bin_boundary[bin_no - 1]:
+            ret_categories.append(bin_no)
+        else:
+            bin_no = bin_no + 1
+            ret_categories.append(bin_no)
+
+
+
+    # return [ret, col_len, quart_len, quart_modulo]
+    return [bin_boundary, ret_categories]
+    # return bin_boundary
