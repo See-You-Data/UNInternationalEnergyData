087c63f5ad7e9b842c213b0c91f3c1d6a21dc933
JBSellman <32461430+JBSellman@users.noreply.github.com>
2021-02-06 15:44:35 +0000

    tidied qcut_james funciton by using linspace, beginning labelling

--- qcut_func.py
+++ qcut_func.py
@@ -1,37 +1,42 @@
 # def qcut_james(x,q,labels):
 
 import math as m
+import numpy as np
 
-def qcut_james(x, q):
+# The purpose of this module is to replicate the qcut function due to issues
+# when calling qcut in modules
+def qcut_james(x, q, labels=[]):
     ret = x.sort_values(ascending=False)
     col_len = len(ret)
-    # col_len = len(x)
     quart_len = m.floor(col_len / q)
     quart_modulo = col_len % q
-    ret_categories = []
 
-    bin_boundary = list()
-    bin_cumulative = 0
-    for i in range(q):
-        if i + 1 <= quart_modulo:
-            bin_size = quart_len + 1
-        else:
-            bin_size = quart_len
 
-        bin_cumulative = bin_cumulative + bin_size
-        bin_boundary.append(bin_cumulative)
+    if (len(labels) != q) & (not (not labels)):
+        return "labels should be the same length as q"
 
+    # utilizing linspace creates quick boundaries for the grouping function
+    bin_boundary = np.linspace(0, col_len, q + 1).astype(int)
 
     bin_no = 1
+    ret_categories = []
     for i in range(col_len):
-        if i <= bin_boundary[bin_no - 1]:
+        if i <= bin_boundary[bin_no]:
             ret_categories.append(bin_no)
         else:
             bin_no = bin_no + 1
             ret_categories.append(bin_no)
 
+
+    ret = ret.to_frame()
+    ret = ret.assign(
+        category = ret_categories
+    )
+
+
+
     # ret.columns()
-    
     # return [ret, col_len, quart_len, quart_modulo]
-    return ret.columns()
+    # return ret.columns()
     # return [bin_boundary, ret_categories]
+    return [ret]
